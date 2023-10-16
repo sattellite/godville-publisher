0fd4f5a986fa9b27cbbe7e90cf653e36c5d7e674
Alexander Groshev <null@sattellite.me>
2023-10-16 09:04:20 +0300

    feat: added mastodon options to config

--- internal/config/config.go
+++ internal/config/config.go
@@ -8,6 +8,10 @@ import (
 type Config struct {
 	GodvilleGodname string
 	GodvilleToken   string
+
+	MastodonServer string
+	MastodonID     string
+	MastodonSecret string
 }
 
 func Load() (*Config, error) {
@@ -19,8 +23,22 @@ func Load() (*Config, error) {
 	if godvilleToken == "" {
 		return nil, errors.New("GODVILLE_TOKEN is not set")
 	}
+
+	// get mastodon config
+	mastodonServer := os.Getenv("MASTODON_SERVER")
+	mastodonID := os.Getenv("MASTODON_ID")
+	mastodonSecret := os.Getenv("MASTODON_SECRET")
+
+	if mastodonServer != "" && (mastodonID == "" || mastodonSecret == "") {
+		return nil, errors.New("MASTODON_ID and MASTODON_SECRET must be set if MASTODON_SERVER is set")
+	}
+
 	return &Config{
 		GodvilleGodname: godvilleGodname,
 		GodvilleToken:   godvilleToken,
+
+		MastodonServer: mastodonServer,
+		MastodonID:     mastodonID,
+		MastodonSecret: mastodonSecret,
 	}, nil
 }
