42dbc5d5e12855c60315bdff604b02d5f77c9294
Alexander Groshev <null@sattellite.me>
2023-12-21 17:50:58 +0300

    feat: added flags to select what to do

--- cmd/godville/main.go
+++ cmd/godville/main.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	"context"
+	"flag"
+	"log"
 
 	"github.com/sattellite/godville-publisher/internal/config"
 	"github.com/sattellite/godville-publisher/internal/godville"
 	"github.com/sattellite/godville-publisher/internal/publisher/mastodon"
-
-	"github.com/davecgh/go-spew/spew"
 )
 
 func main() {
+	var isSendMessage bool
+	var isUpdateProfile bool
+	flag.BoolVar(&isSendMessage, "m", false, "send last dairy message")
+	flag.BoolVar(&isUpdateProfile, "p", false, "update profile from last game info")
+	flag.Parse()
+
 	cfg, cfgErr := config.Load()
 	if cfgErr != nil {
 		panic(cfgErr)
@@ -36,6 +42,17 @@ func main() {
 		panic(err)
 	}
 
-	spew.Dump(publisher)
-	spew.Dump(info)
+	if isSendMessage {
+		msgErr := publisher.SendMessageWithoutDuplicate(ctx, info.DiaryLast)
+		if msgErr != nil {
+			log.Printf("failed send message: %s\n", msgErr.Error())
+		}
+	}
+
+	if isUpdateProfile {
+		profErr := publisher.UpdateProfile(ctx, info)
+		if profErr != nil {
+			log.Printf("failed update profile: %s", profErr.Error())
+		}
+	}
 }
