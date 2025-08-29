da598d5c18bd1cbe77a6c19e8c45712b279ac595
Alexander Groshev <null@sattellite.me>
2025-08-29 17:36:43 +0300

    fix(client): remove debug print statements from HTTP proxy functions

--- internal/client/client.go
+++ internal/client/client.go
@@ -2,7 +2,6 @@ package client
 
 import (
 	"errors"
-	"fmt"
 	"io"
 	"math/rand/v2"
 	"net/http"
@@ -14,14 +13,11 @@ import (
 const proxyList = "https://raw.githubusercontent.com/vakhov/fresh-proxy-list/refs/heads/master/http.txt"
 
 func HTTP(withProxy bool) *http.Client {
-	fmt.Println("With proxy:", withProxy)
 	if withProxy {
 		proxyURL, err := getProxyURL()
 		if err != nil {
-			fmt.Println("Failed to get proxy URL:", err)
 			return http.DefaultClient
 		}
-		fmt.Println("Using proxy:", proxyURL)
 		return &http.Client{
 			Transport: &http.Transport{
 				Proxy: http.ProxyURL(proxyURL), // use environment proxy settings
@@ -32,10 +28,8 @@ func HTTP(withProxy bool) *http.Client {
 }
 
 func validateProxy(p string) bool {
-	fmt.Println("Validating proxy:", p)
 	pu, err := url.Parse("http://" + p)
 	if err != nil {
-		fmt.Println("Failed to parse proxy URL:", err)
 		return false
 	}
 
@@ -48,12 +42,10 @@ func validateProxy(p string) bool {
 
 	resp, err := c.Get("https://godville.net/gods/api/admin")
 	if err != nil {
-		fmt.Println("Proxy error:", err)
 		return false
 	}
 	resp.Body.Close()
 
-	fmt.Println("Proxy status code:", resp.StatusCode)
 	return resp.StatusCode == http.StatusOK
 }
 
@@ -66,7 +58,6 @@ func getProxyURL() (*url.URL, error) {
 
 	body, readErr := io.ReadAll(resp.Body)
 	if readErr != nil {
-		fmt.Println("Failed to read proxy list:", readErr)
 		return nil, readErr
 	}
 
@@ -75,7 +66,6 @@ func getProxyURL() (*url.URL, error) {
 		// remove control characters
 		list = append(list, strings.Trim(line, "\r\n\t "))
 	}
-	fmt.Println("Loaded proxies:", len(list))
 
 	// select a random proxy from the list
 	p := randomItem(list)
